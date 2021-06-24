01d38a29723a6d7d1b2e5ecae4dc0651acfbc3c5
Lucapar45 <69585105+Lucapar45@users.noreply.github.com>
2021-06-24 22:25:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Michela Marinai Site
+ 
---
+++ code.js
@@ -0,0 +1,50 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById('myDropdown2').classList.remove('show2');
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show2");
+    document.getElementById('myDropdown').classList.remove('show');
+}
+
+function myFunction3(x) {
+    x.classList.toggle("change");
+  }
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById('footer').style.width = '85%';
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById('footer').style.width = '98%';
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function openPage(pageName, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    var color = rgb(109, 62, 250);
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
